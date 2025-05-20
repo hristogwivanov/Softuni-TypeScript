0c4130a77075ad90b782ef25b0447ed5f656f114
Hristo Ivanov <105433871+hristogwivanov@users.noreply.github.com>
2025-05-20 15:06:56 +0300

    Initial commit

---
+++ 01.Introduction to TypeScript/presentation.ts
@@ -0,0 +1,16 @@
+let str: string = 'hello';
+str = 'singleQuotes' ;
+str = "doubleQuotes" ;
+// str = 11;
+
+let decimal: number = 11;
+let hex: number = 7E3;
+let binary: number = 11111100011;
+let float: number = 3.14;
+// decimal = 'hello';
+
+let isBool: boolean = true; 
+isBool = 5 < 2;
+let numbers = [1, 2, 3, 4];
+isBool = numbers.includes(100);
+// usBool = 11;
