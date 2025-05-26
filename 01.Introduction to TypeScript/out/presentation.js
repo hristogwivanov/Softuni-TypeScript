8243dc44cc3bb9ab37d9ed6301db88f26b387e06
Hristo Ivanov <gw.hivanov@gmail.com>
2025-05-26 12:28:13 +0300

    Type inference added

--- 01.Introduction to TypeScript/out/presentation.js
+++ 01.Introduction to TypeScript/out/presentation.js
@@ -1,10 +1,10 @@
 "use strict";
-let str = 'hello';
-str = 'singleQuotes';
+let str = "hello";
+str = "singleQuotes";
 str = "doubleQuotes";
 // str = 11;
 let decimal = 11;
-let hex = 7E3;
+let hex = 7e3;
 let binary = 11111100011;
 let float = 3.14;
 // decimal = 'hello';
@@ -13,38 +13,54 @@ isBool = 5 < 2;
 let numbers = [1, 2, 3, 4];
 isBool = numbers.includes(100);
 // usBool = 11;
-let uniqueSymbol = Symbol('mySymbol');
-let anotherSymbol = Symbol('mySymbol');
+let uniqueSymbol = Symbol("mySymbol");
+let anotherSymbol = Symbol("mySymbol");
 console.log(uniqueSymbol === anotherSymbol);
 let undefinedValue1;
 let undefinedValue2 = undefined;
 let person = null;
 let arrayOfStr = [];
-arrayOfStr.push('Hello'); // valid
+arrayOfStr.push("Hello"); // valid
 // arrayOfStr.push(11);
 let tuple;
-tuple = ['Hello', 11];
+tuple = ["Hello", 11];
 // tuple = [11, 'Hello'];
 var DaysOfTheWeek;
 (function (DaysOfTheWeek) {
     DaysOfTheWeek[DaysOfTheWeek["Monday"] = 1] = "Monday";
     DaysOfTheWeek[DaysOfTheWeek["Tuesday"] = 2] = "Tuesday";
 })(DaysOfTheWeek || (DaysOfTheWeek = {}));
-;
 let day;
 day = DaysOfTheWeek.Monday;
 console.log(day);
 if (day === DaysOfTheWeek.Monday) {
-    console.log('I hope you all had a great weekend!');
+    console.log("I hope you all had a great weekend!");
 }
-let a = 'hello';
-let b = 'hello';
+let a = "hello";
+let b = "hello";
 a = 11;
 b = 12;
 console.log(a.length);
 // console.log(b.length);
-greet('message string');
-function greet(message) {
-    console.log(message);
+greet("message string");
+// function greet(message: string): void {
+//     console.log(message);
+// }
+function optionalParams(name, mail) {
+    // logic
+} //valid
+// function optionalParams(name?: string, mail. string){
+//     //logiic
+// }// invalid
+//Return data types
+function greet(name) {
+    return name;
 }
+console.log(greet('Hello'));
+//Type Inference
+//Type inference allows TypeScript to automatically deduce types, improving readability and development speed
+let httpCode = {
+    code: 404,
+    text: 'Page not found'
+};
 //# sourceMappingURL=presentation.js.map
--- 01.Introduction to TypeScript/presentation.ts
+++ 01.Introduction to TypeScript/presentation.ts
@@ -1,58 +1,83 @@
-let str: string = 'hello';
-str = 'singleQuotes' ;
-str = "doubleQuotes" ;
+let str: string = "hello";
+str = "singleQuotes";
+str = "doubleQuotes";
 // str = 11;
 
 let decimal: number = 11;
-let hex: number = 7E3;
+let hex: number = 7e3;
 let binary: number = 11111100011;
 let float: number = 3.14;
 // decimal = 'hello';
 
-let isBool: boolean = true; 
+let isBool: boolean = true;
 isBool = 5 < 2;
 let numbers = [1, 2, 3, 4];
 isBool = numbers.includes(100);
 // usBool = 11;
 
-let uniqueSymbol: symbol = Symbol('mySymbol');
-let anotherSymbol: symbol = Symbol( 'mySymbol');
+let uniqueSymbol: symbol = Symbol("mySymbol");
+let anotherSymbol: symbol = Symbol("mySymbol");
 console.log(uniqueSymbol === anotherSymbol);
 
 let undefinedValue1;
-let undefinedValue2: undefined = undefined; 
+let undefinedValue2: undefined = undefined;
 let person: null = null;
 
-let arrayOfStr: string[]=[];
-arrayOfStr.push('Hello'); // valid
+let arrayOfStr: string[] = [];
+arrayOfStr.push("Hello"); // valid
 // arrayOfStr.push(11);
 
 let tuple: [string, number];
-tuple = ['Hello', 11]; 
+tuple = ["Hello", 11];
 // tuple = [11, 'Hello'];
 
-
-enum DaysOfTheWeek{
+enum DaysOfTheWeek {
     Monday = 1,
     Tuesday,
-};
+}
 
 let day: DaysOfTheWeek;
 day = DaysOfTheWeek.Monday;
 console.log(day);
 if (day === DaysOfTheWeek.Monday) {
-    console.log('I hope you all had a great weekend!');
+    console.log("I hope you all had a great weekend!");
 }
 
-let a: any = 'hello';
-let b: unknown = 'hello';
-a = 11; 
+let a: any = "hello";
+let b: unknown = "hello";
+a = 11;
 b = 12;
 console.log(a.length);
 // console.log(b.length);
 
-greet('message string');
+greet("message string");
 
-function greet(message: string): void {
-    console.log(message);
+// function greet(message: string): void {
+//     console.log(message);
+// }
+
+function optionalParams(name: string, mail?: string) {
+    // logic
+} //valid
+
+// function optionalParams(name?: string, mail. string){
+//     //logiic
+// }// invalid
+
+
+//Return data types
+
+function greet (name: string): string {
+    return name; 
 }
+
+console.log(greet('Hello'));
+
+//Type Inference
+//Type inference allows TypeScript to automatically deduce types, improving readability and development speed
+
+let httpCode = {
+    code: 404, 
+    text: 'Page not found'
+};
+
