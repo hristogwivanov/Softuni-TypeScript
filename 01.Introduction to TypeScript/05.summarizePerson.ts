9137be9de690800a870c7146f02464e0c7862374
Hristo Ivanov <105433871+hristogwivanov@users.noreply.github.com>
2025-05-27 15:51:39 +0300

    Task 5 completed

---
+++ 01.Introduction to TypeScript/05.summarizePerson.ts
@@ -0,0 +1,26 @@
+function summarizePerson(
+    id: number,
+    firstName:string,
+    lastName: string, 
+    age: number, 
+    middleName?:string,
+    hobbies?: string[],
+    workinfo?: [string, number]
+) :[id: number, fullName: string, age: number, hobbyList: string, workinfoStr: string]{
+    let fullName = "";
+    middleName
+    ? fullName = `${firstName} ${middleName} ${lastName}`    
+    : fullName = `${firstName} ${lastName}}`
+    let hobbyList: string;
+    hobbies
+    ? hobbyList=hobbies.join(", ")
+    : hobbyList = "-";
+    const workinfoStr = workinfo? `${workinfo[0]} -> ${workinfo[1]}` : '-';
+
+    return [id, fullName, age, hobbyList, workinfoStr];
+}
+
+console.log(summarizePerson(12, 'Eliot', 'Des', 20, 'Braylen', ['tennis', 'football', 'hiking'], ['Sales Consultant', 2500]))
+console.log(summarizePerson(20, 'Mary', 'Trent', 25, undefined, ['fitness', 'rowing']))
+console.log(summarizePerson(21, 'Joseph', 'Angler', 28))
+console.log(summarizePerson(21, 'Kristine', 'Neva', 23, ''))
---
+++ 01.Introduction to TypeScript/out/05.summarizePerson.js
@@ -0,0 +1,18 @@
+"use strict";
+function summarizePerson(id, firstName, lastName, age, middleName, hobbies, workinfo) {
+    let fullName = "";
+    middleName
+        ? fullName = `${firstName} ${middleName} ${lastName}`
+        : fullName = `${firstName} ${lastName}}`;
+    let hobbyList;
+    hobbies
+        ? hobbyList = hobbies.join(", ")
+        : hobbyList = "-";
+    const workinfoStr = workinfo ? `${workinfo[0]} -> ${workinfo[1]}` : '-';
+    return [id, fullName, age, hobbyList, workinfoStr];
+}
+console.log(summarizePerson(12, 'Eliot', 'Des', 20, 'Braylen', ['tennis', 'football', 'hiking'], ['Sales Consultant', 2500]));
+console.log(summarizePerson(20, 'Mary', 'Trent', 25, undefined, ['fitness', 'rowing']));
+console.log(summarizePerson(21, 'Joseph', 'Angler', 28));
+console.log(summarizePerson(21, 'Kristine', 'Neva', 23, ''));
+//# sourceMappingURL=05.summarizePerson.js.map
